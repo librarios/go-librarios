e12f8f3b6edf9509f39903ec8ac9542236b4c540
lechuck <member@lechuck2011.cafe24.com>
2020-03-14 18:34:54 +0900

    set CORS

--- app/controller/init.go
+++ app/controller/init.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/librarios/go-librarios/app/service"
 	"log"
@@ -12,10 +13,15 @@ func InitEndpoints(
 	bookService service.IBookService,
 ) {
 	r := gin.Default()
+	setCORS(r)
 	addEndpoints(r, bookService)
 	if err := r.Run(fmt.Sprintf(":%d", port)); err != nil {
 		log.Panicf("failed to start server on %d port. error: %v", port, err)
 	}
+}
+
+func setCORS(r *gin.Engine) {
+	r.Use(cors.Default())
 }
 
 func addEndpoints(r *gin.Engine, bookService service.IBookService) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/librarios/go-librarios
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
